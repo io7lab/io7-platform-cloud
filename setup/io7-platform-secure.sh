2f180d2d3e15c329cc64b5bef3f8baa06720465c
Yoonseok Hur <yoonseok@gmail.com>
2023-07-20 12:55:53 +0900

    to use the pre created certificate file

--- setup/io7-platform-secure.sh
+++ setup/io7-platform-secure.sh
@@ -1,3 +1,8 @@
+ssl_file_base=""
+if [ "$1" != "" ]
+then
+    ssl_file_base=$(echo $1| awk -F"." '{print $1}')
+fi
 dir=$(dirname $(echo $0))
 
 docker-compose -f ~/docker-compose.yml down
@@ -15,8 +20,15 @@ then
 fi
 sed -i $sedOpt 's/ws:/wss:/' ~/data/io7-management-web/public/runtime-config.js
 
-mkdir ~/data/certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/data/certs/iothub.key -out ~/data/certs/iothub.crt
+mkdir -p ~/data/certs
+if [ "$ssl_file_base" = "" ]
+then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/data/certs/iothub.key -out ~/data/certs/iothub.crt
+else
+    echo cp $ssl_file_base.crt ~/data/certs/iothub.crt
+    echo cp $ssl_file_base.key ~/data/certs/iothub.key
+fi
+
 mkdir ~/data/io7-management-web/certs
 cp ~/data/certs/* ~/data/io7-management-web/certs
 mkdir ~/data/io7-api-server/certs
