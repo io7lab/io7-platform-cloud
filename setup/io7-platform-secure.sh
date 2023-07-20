e419e71ad07d0a414d2ed91c9cb9765ed5c37318
Yoonseok Hur <yoonseok@gmail.com>
2023-07-20 20:14:55 +0900

    io7-platform-secure.sh

--- setup/io7-platform-secure.sh
+++ setup/io7-platform-secure.sh
@@ -3,6 +3,16 @@ if [ "$1" != "" ]
 then
     ssl_file_base=$(echo $1| awk -F"." '{print $1}')
 fi
+
+mkdir -p ~/data/certs
+if [ "$ssl_file_base" = "" ]
+then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/data/certs/iothub.key -out ~/data/certs/iothub.crt
+else
+    cp $ssl_file_base.crt ~/data/certs/iothub.crt
+    cp $ssl_file_base.key ~/data/certs/iothub.key
+fi
+
 dir=$(dirname $(echo $0))
 
 docker-compose -f ~/docker-compose.yml down
@@ -20,15 +30,6 @@ then
 fi
 sed -i $sedOpt 's/ws:/wss:/' ~/data/io7-management-web/public/runtime-config.js
 
-mkdir -p ~/data/certs
-if [ "$ssl_file_base" = "" ]
-then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/data/certs/iothub.key -out ~/data/certs/iothub.crt
-else
-    cp $ssl_file_base.crt ~/data/certs/iothub.crt
-    cp $ssl_file_base.key ~/data/certs/iothub.key
-fi
-
 mkdir -p ~/data/io7-management-web/certs
 cp ~/data/certs/* ~/data/io7-management-web/certs || sudo cp ~/data/certs/* ~/data/io7-management-web/certs
 # if the ownership of the cert files has changed, then sudo cp
@@ -36,6 +37,6 @@ mkdir -p ~/data/io7-api-server/certs
 cp ~/data/certs/* ~/data/io7-api-server/certs || sudo cp ~/data/certs/* ~/data/io7-api-server/certs
 mkdir -p ~/data/nodered/certs
 cp ~/data/certs/* ~/data/nodered/certs || sudo cp ~/data/certs/* ~/data/nodered/certs
-mkdir -p ~/data/influxdb/certs
+mkdir -p ~/data/influxdb/certs || sudo mkdir -p ~/data/influxdb/certs
 cp ~/data/certs/* ~/data/influxdb/certs || sudo cp ~/data/certs/* ~/data/influxdb/certs
 docker-compose -f ~/docker-compose.yml up -d
