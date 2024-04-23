1ff30ce097529509d0790fd97288aca579b319cc
Yoonseok Hur <yoonseok@gmail.com>
2024-04-23 11:01:56 +0900

    modify-docker-compose.js and shells

--- develop/modify-docker-compose.js
+++ develop/modify-docker-compose.js
@@ -1,58 +1,52 @@
 const { parse, stringify } = require('yaml')
 const fs = require('fs');
+const readline = require('readline');
 
 if (process.argv.length < 3) {
-    console.log("Usage: node modify-docker-compose.js <docker-compose.yml> <cafile=none>");
+    console.log("\nUsage: node modify-docker-compose.js <docker-compose.yml>");
+    console.log("and then enter the key value pairs to add or remove as follows:\n");
+    console.log("services.mqtt.ports 8883:8883");
+    console.log("\t// this adds a new port mapping 8883:8883");
+    console.log("services.mqtt.ports 1883:1883 -");
+    console.log("\t// here '-' means remove this key value pair");
     process.exit(1);
 }
 
+let rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
 let file = process.argv[2];
-let cafile = process.argv.length < 4 ? null : process.argv[3];
 
 let data = fs.readFileSync(file, 'utf8');
 let jo = parse(data)
+let cmd ='';
 
-if (cafile !== null) {
-    jo.services.mqtt.ports = jo.services.mqtt.ports.filter((p) => ! p.includes('1883:'));
-    jo.services.mqtt.ports.push('8883:8883');
-    
-    jo.services.nodered.volumes.push('./data/certs:/data/certs');
-    
-    jo.services.influxdb.environment.push('INFLUXD_TLS_CERT=/data/certs/iothub.crt');
-    jo.services.influxdb.environment.push('INFLUXD_TLS_KEY=/data/certs/iothub.key');
-    jo.services.influxdb.volumes.push('./data/certs:/data/certs');
-    
-    if (!jo.services.io7api.hasOwnProperty('environemnt')) jo.services.io7api.environment = [];
-    jo.services.io7api.environment.push('MQTT_CONN=mqtts://mqtt');
-    jo.services.io7api.environment.push('SSL_CERT=./certs/iothub.crt');
-    jo.services.io7api.environment.push('SSL_KEY=./certs/iothub.key');
-    jo.services.io7api.environment.push(`MQTT_SSL_CERT=./certs/${cafile}`);
-    jo.services.io7api.environment.push('MQTT_PORT=8883');
-    jo.services.io7api.volumes.push('./data/certs:/app/certs');
-    
-    jo.services.io7web.volumes.push('./data/certs:/home/node/app/certs');
-    
-    jo.services.grafana.environment.push('GF_SERVER_PROTOCOL=https');
-    jo.services.grafana.environment.push('GF_SERVER_CERT_FILE=/var/lib/grafana/certs/iothub.crt');
-    jo.services.grafana.environment.push('GF_SERVER_CERT_KEY=/var/lib/grafana/certs/iothub.key');
-} else {
+rl.on('line', data => {
 
-    jo.services.mqtt.ports = jo.services.mqtt.ports.filter((p) => ! p.includes('8883:'));
-    jo.services.mqtt.ports.push('1883:1883');
-    
-    jo.services.nodered.volumes = jo.services.nodered.volumes.filter((v)=> !v.includes('./data/certs:/data/certs'));
-    
-    jo.services.influxdb.environment = jo.services.influxdb.environment.filter((e) => !e.includes('INFLUXD_TLS_CERT'));
-    jo.services.influxdb.environment = jo.services.influxdb.environment.filter((e) => !e.includes('INFLUXD_TLS_KEY'));
-    jo.services.influxdb.volumes = jo.services.influxdb.volumes.filter((v) => ! v.includes('./data/certs:/data/certs'));
+    data = data.trim();
+    if (data.length > 1) {              // if data is empty, ignore
+        let remove = data[data.length - 1] === '-' ? true : false;
+        if (remove) {
+            data = data.slice(0, -1).trim();
+        }
+        let blank = data.indexOf(' ');
+        let key = data.slice(0, blank);
+        let value = data.slice(blank + 1);
+        
+        if (remove) {
+            cmd = `try {jo.${key}=jo.${key}.filter((e) => ! e.includes('${value}'));`;
+            cmd += `if (jo.${key}.length === 0) delete jo.${key};`;
+            cmd += '} catch(e) {};'
+            eval(cmd);
+        } else {
+            cmd = `try { if (jo.${key} === undefined) jo.${key} =[];`;
+            cmd += `let dup = jo.${key}.filter((e) => e.includes('${value}'));`;
+            cmd += `if (dup.length === 0) jo.${key}.push('${value}');`;
+            cmd += '} catch(e) {};'
+            eval(cmd);
+        }
+    }
+});
 
-    delete jo.services.io7api['environment'];
-    jo.services.io7api.volumes = jo.services.io7api.volumes.filter((p) => !p.includes('./data/certs:/app/certs'));
-
-    jo.services.io7web.volumes = jo.services.io7web.volumes.filter((p) => !p.includes('./data/certs:/home/node/app/certs'));
-    
-    jo.services.grafana.environment = jo.services.grafana.environment.filter((e) => !e.includes('GF_SERVER_PROTOCOL'));
-    jo.services.grafana.environment = jo.services.grafana.environment.filter((e) => !e.includes('GF_SERVER_CERT_FILE'));
-    jo.services.grafana.environment = jo.services.grafana.environment.filter((e) => !e.includes('GF_SERVER_CERT_KEY'));
-}
-fs.writeFileSync(file, stringify(jo))
+rl.on('close', function() {
+    fs.writeFileSync(file, stringify(jo))
+});
--- setup/io7-platform-nonsecure.sh
+++ setup/io7-platform-nonsecure.sh
@@ -10,7 +10,24 @@ docker compose -f ~/docker-compose.yml down
 
 dir=$(pwd)/$(dirname $(echo $0))
 cp ~/docker-compose.yml ~/docker-compose.yml.ssl
-node $dir/modify-docker-compose.js ~/docker-compose.yml
+node $dir/modify-docker-compose.js ~/docker-compose.yml <<EOF
+services.mqtt.ports 8883:8883 -
+services.mqtt.ports 1883:1883
+services.nodered.volumes ./data/certs:/data/certs -
+services.influxdb.environment INFLUXD_TLS_CERT -
+services.influxdb.environment INFLUXD_TLS_KEY -
+services.influxdb.volumes ./data/certs:/data/certs -
+services.io7api.environment MQTT_PORT -
+services.io7api.environment MQTT_CONN -
+services.io7api.environment SSL_CERT -
+services.io7api.environment SSL_KEY -
+services.io7api.environment MQTT_SSL_CERT=./cer -
+services.io7api.volumes ./data/certs:/app/certs -
+services.io7web.volumes ./data/certs:/home/node/app/certs -
+services.grafana.environment GF_SERVER_PROTOCOL -
+services.grafana.environment GF_SERVER_CERT_FILE -
+services.grafana.environment GF_SERVER_CERT_KEY -
+EOF
 
 cp $dir/../data/nodered/settings.js ~/data/nodered/settings.js
 sudo cp $dir/../data/mosquitto/config/mosquitto.conf ~/data/mosquitto/config/mosquitto.conf
