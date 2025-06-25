4df2c3a895ef296e7c104b6651c7df128ed1b52e
Yoonseok Hur <yoonseok@gmail.com>
2025-06-26 07:14:29 +0900

    monitored_devices/fieldsets key init

--- setup/setup_grafana_dashboard.sh
+++ setup/setup_grafana_dashboard.sh
@@ -129,6 +129,8 @@ dashboard_uid=$(curl -s -X POST "http://localhost:3003/api/dashboards/db" \
 # adding configuration parameters
 add_config_var influxdb_token "$influxdb_token"
 add_config_var gf_token "$gf_token"
+add_config_var monitored_devices "*"
+add_config_var monitored_fieldsets "humidity, lux"
 echo Making the default dashboard public
 # making the default dashboard public
 accessToken=$(curl -X POST \
